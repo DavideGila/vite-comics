da06ad851d973e955f3313f5613d24e9d2056a16
DavideGila <davide.gila@gmail.com>
2023-11-09 19:34:09 +0100

    created the last part of the footer and gave style

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,7 +1,7 @@
 <template>
-    <div>
+    <div class="bg-main">
         <div class="container d-flex">
-            <div>
+            <div class="py-4 pe-3">
                 <h4>DC COMICS</h4>
                 <ul class="p-0">
                     <li v-for="element in first"><a href="">{{ element }}</a></li>
@@ -11,17 +11,33 @@
                     <li v-for="element in second"><a href="">{{ element }}</a></li>
                 </ul>
             </div>
-            <div class="ps-3">
+            <div class="py-4 pe-3">
                 <h4>DC</h4>
                 <ul class="p-0">
                     <li v-for="element in third"><a href="">{{ element }}</a></li>
                 </ul>
             </div>
-            <div class="ps-3">
+            <div class="py-4 pe-3">
                 <h4>SITES</h4>
                 <ul class="p-0">
                     <li v-for="element in fourth"><a href="">{{ element }}</a></li>
                 </ul>
+            </div>
+            <div class="bg-main-logo"></div>
+        </div>
+    </div>
+    <div class="bg-grey py-4">
+        <div class="container d-flex justify-content-between align-items-center p-0">
+            <div>
+                <button class="btn btn-primary">SIGN-UP NOW</button>
+            </div>
+            <div class="d-flex">
+                <h4 class="pe-4">FOLLOW US</h4>
+                <img src="/public/img/footer-facebook.png" alt="" class="pe-4">
+                <img src="/public/img/footer-twitter.png" alt="" class="pe-4">
+                <img src="/public/img/footer-youtube.png" alt="" class="pe-4">
+                <img src="/public/img/footer-pinterest.png" alt="" class="pe-4">
+                <img src="/public/img/footer-periscope.png" alt="">
             </div>
         </div>
     </div>
@@ -74,7 +90,7 @@
 <style lang="scss" scoped>
 a {
         text-decoration: none;
-        color: white;
+        color: grey;
     }
     li {
         list-style: none;
@@ -83,6 +99,16 @@ a {
         color: white;
     }
     .bg-main {
-        background-image: url('img/')
+        background-image: url('img/footer-bg.jpg');
+        background-size: cover;
+    }
+    .bg-main-logo {
+        background-image: url('/img/dc-logo-bg.png');
+        background-position: right;
+        background-repeat: no-repeat;
+        width: 100%;
+    }
+    .bg-grey {
+        background-color: rgb(53, 53, 53)
     }
 </style>
