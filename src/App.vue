efb734c890d2e22e7609bb23a64d56ba551c1dce
DavideGila <davide.gila@gmail.com>
2023-11-09 16:04:30 +0100

    imported header, main, footer

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <AppHeader />
+    <AppMainContent />
+    <AppFooter />
+  </div>
+  
+</template>
+
+<script>
+  import AppHeader from './components/AppHeader.vue'
+  import AppMainContent from './components/AppMainContent.vue'
+  import AppFooter from './components/AppFooter.vue'
+
+  export default {
+    components: {
+      AppHeader,
+      AppMainContent,
+      AppFooter
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
