d849e6dcf73031e2ff9b592244799268c48a2ca1
DavideGila <davide.gila@gmail.com>
2023-11-09 16:05:35 +0100

    created navbar

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="d-flex justify-content-between">
+        <img src="../../public/img/dc-logo.png" alt="Logo-DC">
+        <div>
+        <ul class="d-flex">
+            <li v-for="nav in navBar"><a href="">{{ nav }}</a></li>
+        </ul>
+        </div>
+    </div>   
+</template>
+
+<script>
+    export default {
+        nome: 'AppHeader',
+        data() {
+            return {
+                navBar: [
+                    'Characters',
+                    'Comics',
+                    'Movies',
+                    'TV',
+                    'Games',
+                    'Collectibles',
+                    'Videos',
+                    'Fans',
+                    'News',
+                    'Shop'
+                ]
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
