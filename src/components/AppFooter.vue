d620ac9b6616bb11f72022ee772799a0bf4df797
DavideGila <davide.gila@gmail.com>
2023-11-09 16:05:09 +0100

    added footer placeholder

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        Footer
+    </div>
+</template>
+
+<script>
+    export default {
+        nome: 'AppFooter'
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
