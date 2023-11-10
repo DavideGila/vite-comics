7226a891f1a619dbcdf2df6f38b9856ecab89bd1
DavideGila <davide.gila@gmail.com>
2023-11-10 16:35:38 +0100

    created props for the cards in the main

---
+++ src/components/main/Props.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="col-2">
+        <div>
+            <img :src="img" :alt="title"/>
+            <div>
+                <h6 class="title text-light">{{ title }}</h6>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    nome: 'Props',
+    props: {
+        img: String,
+        title: String
+    }
+
+}
+</script>
+
+<style lang="scss" scoped>
+img {
+  width: 200px;
+  height: 200px;
+}
+.title {
+  width: 90%;
+  padding: 5px 0;
+}
+</style>
