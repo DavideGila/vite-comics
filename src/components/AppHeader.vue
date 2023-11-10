bd90e8f5d0e82111816f3b5901f1696e9ae0a03a
DavideGila <davide.gila@gmail.com>
2023-11-10 18:10:35 +0100

    created jumbotron

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,43 +1,53 @@
 <template>
-    <div class="d-flex justify-content-between align-items-center py-4">
-        <img src="/img/dc-logo.png" alt="Logo-DC">
-        <div>
-        <ul class="d-flex">
-            <li v-for="nav in navBar" class="pe-4"><a href="">{{ nav }}</a></li>
-        </ul>
+    <div class="container">
+        <div class="d-flex justify-content-between align-items-center py-4">
+            <img src="/img/dc-logo.png" alt="Logo-DC">
+            <div>
+                <ul class="d-flex">
+                    <li v-for="nav in navBar" class="pe-4"><a href="">{{ nav }}</a></li>
+                </ul>
+            </div>
         </div>
-    </div>   
+    </div>
+    <div class="jumbotron"></div>
 </template>
 
 <script>
-    export default {
-        nome: 'AppHeader',
-        data() {
-            return {
-                navBar: [
-                    'Characters',
-                    'Comics',
-                    'Movies',
-                    'TV',
-                    'Games',
-                    'Collectibles',
-                    'Videos',
-                    'Fans',
-                    'News',
-                    'Shop'
-                ]
-            }
-        },
-    }
+export default {
+    nome: 'AppHeader',
+    data() {
+        return {
+            navBar: [
+                'Characters',
+                'Comics',
+                'Movies',
+                'TV',
+                'Games',
+                'Collectibles',
+                'Videos',
+                'Fans',
+                'News',
+                'Shop'
+            ]
+        }
+    },
+}
 </script>
 
 <style lang="scss" scoped>
-    li {
-        list-style: none;
-    }
+li {
+    list-style: none;
+}
 
-    a {
-        text-decoration: none;
-        color: black;
-    }
+a {
+    text-decoration: none;
+    color: black;
+}
+
+.jumbotron {
+    height: 400px;
+    background-image: url('/img/jumbotron.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;
+}
 </style>
